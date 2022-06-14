b3fc8e3a2efa1736a5742646a7c7474d419c66d8
nicolas chanon <nicolas.pierre.chanon@cern.ch>
2022-06-14 14:31:01 +0200

    Update README and create dir

---
+++ createDir.sh
@@ -0,0 +1,21 @@
+mkdir -p inclusive/impacts/2016/asimov
+mkdir -p inclusive/impacts/2017/asimov
+mkdir -p inclusive/impacts/Comb/asimov
+mkdir -p inclusive/inputs/2016
+mkdir -p inclusive/inputs/2017
+mkdir -p inclusive/inputs/Comb
+
+mkdir -p one_bin/impacts/2016/
+mkdir -p one_bin/impacts/2017/
+mkdir -p one_bin/impacts/Comb/
+mkdir -p one_bin/inputs/2016
+mkdir -p one_bin/inputs/2017
+mkdir -p one_bin/inputs/Comb
+
+mkdir -p one_bin/impacts/2016/asimov
+mkdir -p one_bin/impacts/2017/asimov
+mkdir -p one_bin/impacts/Comb/asimov
+mkdir -p sme/inputs/2016
+mkdir -p sme/inputs/2017
+mkdir -p sme/inputs/Comb
+
